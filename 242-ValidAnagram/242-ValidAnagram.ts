fa41187b12f6bfe5f92acd1cd928986ece5c5361
AnPQ <63229766+pqan112@users.noreply.github.com>
2025-01-12 22:20:28 +0700

    Update file 1/12/2025, 10:20:27 PM

--- 242-ValidAnagram/242-ValidAnagram.ts
+++ 242-ValidAnagram/242-ValidAnagram.ts
@@ -1,7 +1,8 @@
 function isAnagram(s: string, t: string): boolean {
     const reverseArr = s.split('').sort();
     const targetArr = t.split('').sort();
-    const maxLength = Math.max(s.length, t.length);
+    const maxLength = s.length;
+    if (s.length !== t.length) return false;
 
     for(let i=0; i< maxLength; i++) {
         if(reverseArr[i] !== targetArr[i]) {
