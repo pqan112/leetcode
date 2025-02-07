a80588f46b430025a4dca743ee748767b8461eea
AnPQ <63229766+pqan112@users.noreply.github.com>
2025-02-07 14:14:21 +0700

    Create file

---
+++ 2667-CreateHelloWorldFunction/2667-CreateHelloWorldFunction.ts
@@ -0,0 +1,18 @@
+// function createHelloWorld() {
+//     return function(...args): string {
+//         return "Hello World"
+//     };
+// };
+
+
+function createHelloWorld() {
+    function print(...args): string {
+        return "Hello World"
+    };
+    return print
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
